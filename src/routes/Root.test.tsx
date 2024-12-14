d43ca5ad599f4ee5f4d2d708720d5b085c387d27
thewilley <89783791+TheWilley@users.noreply.github.com>
2024-12-14 20:26:06 +0100

    test: removed uneeded tests

--- src/routes/Root.test.tsx
+++ src/routes/Root.test.tsx
@@ -3,97 +3,68 @@ import { describe, test, expect } from 'vitest';
 import Root from './Root';
 
 describe('Root Component', () => {
-    test('renders the title and subtitle', async () => {
-        await act(async () => {
-            render(<Root />);
-        }); expect(screen.queryByText('csus')).toBeTruthy();
-        expect(screen.queryByText('Client-Side-Url-Shortener')).toBeTruthy();
+  test('renders the title and subtitle', async () => {
+    await act(async () => {
+      render(<Root />);
+    });
+    expect(screen.queryByText('csus')).toBeTruthy();
+    expect(screen.queryByText('Client-Side-Url-Shortener')).toBeTruthy();
+  });
+
+  test('renders the Shortener component by default', async () => {
+    await act(async () => {
+      render(<Root />);
+    });
+    expect(screen.queryByText('Manage your URLs')).toBeTruthy();
+  });
+
+  test('renders the Result component when entering a link and clicking "Shorten"', async () => {
+    await act(async () => {
+      render(<Root />);
     });
 
-    test('renders the Shortener component by default', async () => {
-        await act(async () => {
-            render(<Root />);
-        });
-        expect(screen.queryByText('Manage your URLs')).toBeTruthy();
+    // Enter text into the input field
+    const input = screen.getAllByPlaceholderText('Enter long link here')[0];
+    await act(async () => {
+      fireEvent.change(input, { target: { value: 'https://example.com' } });
     });
 
-    test('renders the Result component when entering a link and clicking "Shorten"', async () => {
-        await act(async () => {
-            render(<Root />);
-        });
-
-        // Enter text into the input field
-        const input = screen.getAllByPlaceholderText('Enter long link here')[0];
-        await act(async () => {
-            fireEvent.change(input, { target: { value: 'https://example.com' } });
-        });
-
-        // Click the shorten button
-        const button = screen.getByText('Shorten');
-        await act(async () => {
-            fireEvent.click(button);
-        });
-
-        const result = await screen.findByText('Shorten Another');
-        expect(result).toBeDefined();
+    // Click the shorten button
+    const button = screen.getByText('Shorten');
+    await act(async () => {
+      fireEvent.click(button);
     });
 
-    test('toggles the dashboard view when the button is clicked', async () => {
-        await act(async () => {
-            render(<Root />);
-        });
-        const button = screen.getByText('Manage your URLs');
-        await act(async () => {
-            fireEvent.click(button);
-        });
+    const result = await screen.findByText('Shorten Another');
+    expect(result).toBeDefined();
+  });
 
-        // Check if the dashboard is shown
-        expect(screen.queryByText('Delete All')).toBeTruthy();
+  test('toggles the dashboard view when the button is clicked', async () => {
+    await act(async () => {
+      render(<Root />);
+    });
+    const button = screen.getByText('Manage your URLs');
+    await act(async () => {
+      fireEvent.click(button);
     });
 
-    test('renders the Footer component when VITE_SHOW_FOOTER is true', async () => {
-        import.meta.env.VITE_SHOW_FOOTER = 'true';
-        await act(async () => {
-            render(<Root />);
-        });
-        expect(screen.queryByText('You can also self-host the app! (GitHub)')).toBeTruthy();
+    // Check if the dashboard is shown
+    expect(screen.queryByText('Delete All')).toBeTruthy();
+  });
+
+  test('renders the Footer component when VITE_SHOW_FOOTER is true', async () => {
+    import.meta.env.VITE_SHOW_FOOTER = 'true';
+    await act(async () => {
+      render(<Root />);
     });
+    expect(screen.queryByText('You can also self-host the app! (GitHub)')).toBeTruthy();
+  });
 
-    test('does not render the Footer component when VITE_SHOW_FOOTER is false', async () => {
-        import.meta.env.VITE_SHOW_FOOTER = 'false';
-        await act(async () => {
-            render(<Root />);
-        });
-        expect(screen.queryByText('You can also self-host the app! (GitHub)')).toBeFalsy();
+  test('does not render the Footer component when VITE_SHOW_FOOTER is false', async () => {
+    import.meta.env.VITE_SHOW_FOOTER = 'false';
+    await act(async () => {
+      render(<Root />);
     });
-
-    test('shows the error message when the input is empty', async () => {
-        await act(async () => {
-            render(<Root />);
-        });
-
-        // Click the shorten button
-        const button = screen.getByText('Shorten');
-        await act(async () => {
-            fireEvent.click(button);
-        });
-
-        const result = screen.queryByText('Invalid URL. Please enter a valid URL.');
-        expect(result).toBeTruthy();
-    });
-
-    test('display the custom UID input when adjusting select', async () => {
-        await act(async () => {
-            render(<Root />);
-        });
-
-        // Select the custom UID option
-        const select = screen.getByTestId('custom-uid-select');
-        await act(async () => {
-            fireEvent.change(select, { target: { value: 'Yes' } });
-        });
-
-        // Check if the custom UID input is shown
-        expect(screen.queryByPlaceholderText('Enter custom UID here')).toBeTruthy();
-    });
+    expect(screen.queryByText('You can also self-host the app! (GitHub)')).toBeFalsy();
+  });
 });
