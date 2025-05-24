11bcbc36c25b843e58dd0222d41128c04cbc7811
thewilley <89783791+TheWilley@users.noreply.github.com>
2025-05-24 20:56:52 +0200

    style: changed height from set to max to allow further expantion estate

--- src/layout/Wrapper.tsx
+++ src/layout/Wrapper.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 function Wrapper(props: Props) {
   return (
-    <div className='flex bg-[#121212] w-screen h-screen text-white font-[JetBrainsMono]'>
+    <div className='flex bg-[#121212] w-screen min-h-screen text-white font-[JetBrainsMono]'>
       <div className='max-w-6xl ml-auto mr-auto mt-8'>
         <div>{props.children}</div>
       </div>
