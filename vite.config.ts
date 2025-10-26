3a8b221aef297d18e5fa38aa9f1506aa57194099
thewilley <89783791+TheWilley@users.noreply.github.com>
2025-10-26 10:19:17 +0100

    refactor: update tailwindcss setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default {
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/csus/',
   envDir: './src',
   test: {
